5f26f18c761e36560d22db4fde92766b6cf5246f
lallhar <lfc5timesktt@gmail.com>
2018-12-11 15:07:05 +0530

    Added bootstrap gem and jquery-rails

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 	http_basic_authenticate_with name: "sri", password: "HarshaK",
-	except: [:index,:show]
+except: [:index,:show]
 
 	def index
 		@articles = Article.all
